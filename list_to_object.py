3042015a3c278a61e27011e01f5aae80971e065b
Caleb Chiam <calebchiam@gmail.com>
2019-04-04 10:33:57 -0400

    film tropes dataset +

--- list_to_object.py
+++ list_to_object.py
@@ -4,11 +4,11 @@ Quick and dirty script for converting JSONList to JSONObject
 import json
 
 retval = dict()
-with open("Literature/tropes_Literature.json", 'r') as f:
+with open("Film/FULL_FILM_TROPES.json", 'r') as f:
     for line in f:
         entry = json.loads(line)
         for title, tropes in entry.items():
             retval[title] = tropes
 
-with open("Literature/Literature_tropes_dataset.json", "w") as f:
+with open("Film/Film_tropes_dataset.json", "w") as f:
     json.dump(retval, f)
