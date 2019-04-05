0dee72aa27d8190f832b68760c182c86b2884c93
Caleb Chiam <calebchiam@gmail.com>
2019-04-05 19:57:05 -0400

    updated successful_books.txt

--- Literature/quick.py
+++ Literature/quick.py
@@ -1,12 +1,21 @@
 import json
+#
+# with open("zerotropes_Literature.json", 'a+') as f1:
+#     with open("fewtropes_Literature.json", "r") as f:
+#         for line in f:
+#             a = json.loads(line)
+#             # print(json.loads(line))
+#             # print(a)
+#             ls = next(iter(a.values()))
+#             if len(ls) == 0:
+#                 f1.write(next(iter(a.keys())))
+#                 f1.write("\n")
 
-with open("zerotropes_Literature.json", 'a+') as f1:
-    with open("fewtropes_Literature.json", "r") as f:
-        for line in f:
-            a = json.loads(line)
-            # print(json.loads(line))
-            # print(a)
-            ls = next(iter(a.values()))
-            if len(ls) == 0:
-                f1.write(next(iter(a.keys())))
-                f1.write("\n")
+
+with open("Literature_tropes_dataset.json", 'r') as f:
+    entries = json.load(f)
+
+with open("../titles/successful_books.txt", 'w') as f:
+    for k in entries:
+        f.write(k)
+        f.write("\n")
