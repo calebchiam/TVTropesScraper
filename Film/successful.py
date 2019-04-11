59012c1b3bb2e2932110a4b7a1821cf4a1885e76
Caleb Chiam <calebchiam@gmail.com>
2019-04-10 22:34:00 -0400

    updated successful movies list

--- Film/successful.py
+++ Film/successful.py
@@ -3,7 +3,7 @@ import json
 with open("Film_tropes_dataset3.json", 'r') as f:
     dataset = json.load(f)
 
-with open("../titles/successful_books2.txt", 'w') as f:
+with open("../titles/successful_movies2.txt", 'w') as f:
     for k in dataset:
         f.write(k)
         f.write("\n")
